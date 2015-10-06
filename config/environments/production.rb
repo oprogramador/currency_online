483a12ff9ec3ae67b9b839d27d68785eb923a944
oprogramador <uomodislesia@gmail.com>
2015-10-06 19:40:58 +0200

    prod assets bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Tshtask::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-   config.assets.precompile += %w( *.js *.css *.css.erb )
+   config.assets.precompile += %w( *.js *.css.erb )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
