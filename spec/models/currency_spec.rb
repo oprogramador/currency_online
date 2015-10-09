1d0027eeb5b5753b8493f36ac3d6be64d44ca108
oprogramador <uomodislesia@gmail.com>
2015-10-09 06:13:20 +0200

    tests

--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Currency do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "median" do
+    it "should be middle value with odd number of elements" do
+      expect(Currency.median [8, 30, 2]).to eql 8
+    end
+
+    it "should be average of two middle values with even number of elements" do
+      expect(Currency.median [4, 11, 30, 3]).to eql 7.5
+    end
+  end
+
+  describe "get_report" do
+    it "should return all needed keys" do
+      Currency.new({code: 'AUD', buy_price: 2, sell_price: 2.5, name: 'AUD', converter: 1}).save!
+      report = Currency.get_report 'AUD'
+      expect(report[:currency]).not_to be_nil
+      expect(report[:all]).not_to be_nil
+      expect(report[:avg_buy_price]).not_to be_nil
+      expect(report[:avg_sell_price]).not_to be_nil
+      expect(report[:median_buy_price]).not_to be_nil
+      expect(report[:median_sell_price]).not_to be_nil
+    end
+  end
 end
--- spec/models/exchange_spec.rb
+++ spec/models/exchange_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Exchange do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should set this to all related currencies" do
+    exchange = Exchange.create!
+    Currency.new({exchange: exchange}).save!
+    exchange.currencies.each do |currency|
+      expect(currency.exchange).to eql(exchange)
+    end
+  end
+
+  describe "save_current_rates" do
+    it "should modify database" do
+      last_modify = Exchange.last.updated_at
+      Exchange.save_current_rates
+      new_last_modify = Exchange.last.updated_at
+      expect(last_modify).not_to eql(new_last_modify)
+    end
+  end
 end
