4e5c59e23d30356b07227b1afeceaf231f2a005a
oprogramador <uomodislesia@gmail.com>
2015-10-09 05:14:58 +0200

    spec update

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,16 +1,11 @@
 require 'spec_helper'
 
-describe HomeController do
+describe HomeController, :type => :controller do
 
   describe "GET 'index'" do
     it "should be successful" do
       get 'index'
-      response.should be_success
-    end
-
-    it "should have the title" do
-      get 'index'
-      page.should have_selector('title', :text => 'Tshtask')
+      expect(response).to be_success
     end
   end
 
