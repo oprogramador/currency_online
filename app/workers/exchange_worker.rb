13b786ec7c74cc935e5104f24806521a430807b9
oprogramador <uomodislesia@gmail.com>
2015-10-13 22:04:10 +0200

    worker bug fix

--- app/workers/exchange_worker.rb
+++ app/workers/exchange_worker.rb
@@ -1,10 +1,10 @@
 class ExchangeWorker
   include Sidekiq::Worker
-  #include Sidetiq::Schedulable
-
-  #recurrence { minutely }
 
   def perform
     Exchange.save_current_rates
+    open('log/exchange_worker.txt', 'a') { |f| f.puts "#{Time.now}\n" }
   end
 end
+
+ExchangeWorker.perform_in 1.day
