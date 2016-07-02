4889ca543eaedd6a937e805cb17dcb2413dda777
oprogramador <uomodislesia@gmail.com>
2016-07-03 00:17:20 +0200

    mailer url config

--- config/environments/common.rb
+++ config/environments/common.rb
@@ -13,4 +13,6 @@ Tshtask::Application.configure do
 
   config.action_mailer.default_url_options = { :host => ENV['DOMAIN_NAME'] }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.asset_host = ENV['DOMAIN_NAME']
+  config.action_mailer.relative_url_root = ENV['DOMAIN_NAME']
 end
