de01a19385770c18b3e4f579a9fa447c939e8bb0
oprogramador <uomodislesia@gmail.com>
2016-07-03 23:14:33 +0200

    required authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Tshtask::Application.routes.draw do
-  root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users, except: [:show, :index]
-  resources :money, except: [:delete, :edit, :update, :create, :new] do
-    collection do
-      post 'refresh_rates'
-      get 'report'
-      get 'all_currencies'
+  authenticate :user do
+    root :to => "home#index"
+    resources :users, except: [:show, :index]
+    resources :money, except: [:delete, :edit, :update, :create, :new] do
+      collection do
+        post 'refresh_rates'
+        get 'report'
+        get 'all_currencies'
+      end
     end
   end
 end
